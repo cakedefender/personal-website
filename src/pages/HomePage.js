db73de3d3d7a6582984cbad0c040df2b32d4145e
Michael Miller <millerallanmichael2020@gmail.com>
2024-10-22 15:39:17 -0500

    finally working

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,9 +1,11 @@
 import React from "react";
+import myself from "../myself.jpg";
 
 const HomePage = () => {
   return (
     <div className="flex flex-col items-center py-10 min-h-screen">
       <h1 className="text-6xl font-medium mb-5">Home Page</h1>
+      <img src={myself} alt="image of myself" className="w-56 my-10"/>
       <div className="w-96">
         {/* <div className="justify-items-center gap-20 p-20 bg-gray-100 text-center"> */}
         <h1 className="font-bold">Hello</h1>
